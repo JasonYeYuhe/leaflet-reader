65bebdb51e62969161ae4d061ca71e626b139a23
user.email <yyyyy.yeyuhe@gmail.com>
2026-04-18 12:35:44 +0900

    auto-dev: Add ReadingLogTests (7 tests) + BookNoteTests (14 tests)

---
+++ GGCReaderTests/ReadingLogTests.swift
@@ -0,0 +1,45 @@
+import XCTest
+@testable import GGCReader
+
+final class ReadingLogTests: XCTestCase {
+
+    // MARK: - pagesRead
+
+    func testPagesReadNormal() {
+        let log = ReadingLog(fromPage: 50, toPage: 100)
+        XCTAssertEqual(log.pagesRead, 50)
+    }
+
+    func testPagesReadSinglePage() {
+        let log = ReadingLog(fromPage: 99, toPage: 100)
+        XCTAssertEqual(log.pagesRead, 1)
+    }
+
+    func testPagesReadZeroWhenEqual() {
+        let log = ReadingLog(fromPage: 50, toPage: 50)
+        XCTAssertEqual(log.pagesRead, 0)
+    }
+
+    func testPagesReadZeroWhenReversed() {
+        // max(toPage - fromPage, 0) guards against negative values
+        let log = ReadingLog(fromPage: 100, toPage: 50)
+        XCTAssertEqual(log.pagesRead, 0)
+    }
+
+    func testPagesReadLargeRange() {
+        let log = ReadingLog(fromPage: 0, toPage: 500)
+        XCTAssertEqual(log.pagesRead, 500)
+    }
+
+    // MARK: - stored fields
+
+    func testFromPageStoredCorrectly() {
+        let log = ReadingLog(fromPage: 42, toPage: 100)
+        XCTAssertEqual(log.fromPage, 42)
+    }
+
+    func testToPageStoredCorrectly() {
+        let log = ReadingLog(fromPage: 42, toPage: 100)
+        XCTAssertEqual(log.toPage, 100)
+    }
+}
